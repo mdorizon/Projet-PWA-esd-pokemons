cd087eec62bd7f1a280d9a56e1e975de4a817bdf
MDorizon <mdorizon@proton.me>
2024-11-22 15:32:39 +0100

    :sparkles: change random pokemon on focus

--- app/pokemons/(tabs)/random/index.tsx
+++ app/pokemons/(tabs)/random/index.tsx
@@ -2,15 +2,27 @@ import PokemonCard from "@/components/PokemonCard";
 import useGetPokemons from "@/hook/useGetPokemons";
 import pokemonListStyles from "@/styles/pokemonList";
 import { PokemonDto } from "@/types/poke-types";
+import { useFocusEffect } from "@react-navigation/native";
+import React, { useState } from "react";
 import { View, ActivityIndicator, Text } from "react-native";
 
 export default function Index() {
   const pokemons: PokemonDto[] = useGetPokemons();
+  const [randomPokemon, setRandomPokemon] = useState<PokemonDto | null>(null);
 
   const getRandomElement = (array: PokemonDto[]) => {
+    if (array.length === 0) return null; // Sécurité supplémentaire
     const randomIndex = Math.floor(Math.random() * array.length);
-    return pokemons[randomIndex];
+    return array[randomIndex];
   };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      if (pokemons && pokemons.length > 0) {
+        setRandomPokemon(getRandomElement(pokemons));
+      }
+    }, [pokemons])
+  );
 
   return (
     <View style={pokemonListStyles.container}>
@@ -20,11 +32,15 @@ export default function Index() {
           <Text style={pokemonListStyles.loaderText}>Chargement du Pokémon...</Text>
         </View>
       ) : (
-          <View style={pokemonListStyles.container}>
-            <View style={pokemonListStyles.list}>
-              <PokemonCard pokemon={getRandomElement(pokemons)} isHorizontal={false}/>
-            </View>
+        <View style={pokemonListStyles.container}>
+          <View style={pokemonListStyles.list}>
+            {randomPokemon ? (
+              <PokemonCard pokemon={randomPokemon} isHorizontal={false} />
+            ) : (
+              <Text style={pokemonListStyles.loaderText}>Aucun Pokémon trouvé.</Text>
+            )}
           </View>
+        </View>
       )}
     </View>
   );
